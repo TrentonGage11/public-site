256f9313a71a0ee48b245d4df80f8e46d9899b1d
Trenton Edwards <39540197+TrentonGage11@users.noreply.github.com>
2020-11-28 23:06:42 -0500

    idek

--- src/js/kltmain.js
+++ src/js/kltmain.js
@@ -1,6 +1,20 @@
+function owl() {
+	sleep(500);
+	console.clear();
+}
 function ol(url) {
 	window.open(url, '_blank');
 }
 function clo() {
 	window.close();
+}
+function opl(name) {
+	document.getElementById(name).scrollIntoView();
+}
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
